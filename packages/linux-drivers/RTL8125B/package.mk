47725878cf2b599b80278429ffff405f3cc283cb
MsV <msv_asb@yahoo.com>
2021-04-29 16:55:05 +0000

    Add precompiled driver RTL8125 for kernel 5.1.16 #1

--- packages/linux-drivers/RTL8125B/package.mk
+++ packages/linux-drivers/RTL8125B/package.mk
@@ -2,31 +2,31 @@
 # Copyright (C) 2021
 
 PKG_NAME="RTL8125B"
-PKG_VERSION="9.005.01"
-PKG_SHA256="6a1e07b0eee5424a197f2b8fa0da25a4ec097550747f0c235c647f390eab6707"
+PKG_VERSION="5.1.16"
+PKG_SHA256="139d1d5f6d863f1b02610de252b0739a5c15bbc53d1ff5e7c4e34dd650ecbd38"
 # PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/awesometic/realtek-r8125-dkms/"
-PKG_URL="http://80.251.144.40/update/RTL8125B-9.005.01.tar.gz"
+PKG_URL="http://80.251.144.40/update/RTL8125B-5.1.16.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
-PKG_LONGDESC="Realtec RTL8125 driver for Linux"
+PKG_LONGDESC="Realtec RTL8125 driver for Linux kernel 5.1.16"
 PKG_IS_KERNEL_PKG="yes"
 
 pre_make_target() {
   unset LDFLAGS
 }
 
-make_target() {
+#make_target() {
 #  kernel_make KDIR=$(kernel_path)
 # cd $PKG_BUILD
 #   bash autorun.sh
-  make V=1 \
-       ARCH=$TARGET_KERNEL_ARCH \
-       KSRC=$(kernel_path) \
-       CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
-       CONFIG_POWER_SAVING=n
-}
+#  make V=1 \
+#       ARCH=$TARGET_KERNEL_ARCH \
+#       KSRC=$(kernel_path) \
+#       CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
+#       CONFIG_POWER_SAVING=n
+#}
 
 makeinstall_target() {
  cd $PKG_BUILD
--- packages/linux-firmware/rtl8125b-firmware/package.mk
+++ packages/linux-firmware/rtl8125b-firmware/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="rtl8125b-firmware"
-PKG_VERSION="rtl8125b"
+PKG_VERSION="0b001"
 PKG_SHA256="1a5094a852aa141f2a281e117b3fbd6bd8888bf70b80c26b107e3bd97cb742e3"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="http://80.251.144.40/LibreELEC/wlan-firmware"
@@ -12,5 +12,5 @@ PKG_LONGDESC="rtl8125b-firmware: firmwares for RTL8125B drivers"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  DESTDIR=$INSTALL/$(get_kernel_overlay_dir) ./install
+  DESTDIR=$INSTALL/$(get_full_firmware_dir)/rtl_nic ./install
 }
