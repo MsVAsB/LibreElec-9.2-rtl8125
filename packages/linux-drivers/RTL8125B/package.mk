9cab3a9357c9a8b116ff31773e6ac09c8305c82c
MsV <msv_asb@yahoo.com>
2021-04-30 18:01:01 +0000

    Last update package.mk with options for build

--- packages/linux-drivers/RTL8125B/package.mk
+++ packages/linux-drivers/RTL8125B/package.mk
@@ -2,31 +2,21 @@
 # Copyright (C) 2021
 
 PKG_NAME="RTL8125B"
-PKG_VERSION="5.1.16"
-PKG_SHA256="139d1d5f6d863f1b02610de252b0739a5c15bbc53d1ff5e7c4e34dd650ecbd38"
-# PKG_ARCH="x86_64"
+PKG_VERSION="cb398363f11d742d880b8bb80763d050f00e7e98"
+PKG_SHA256="8f05052c59e69a8bf0ada420d25ef29f54a38d25e0fd841e9df9e61145a48f55"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/awesometic/realtek-r8125-dkms/"
-PKG_URL="http://80.251.144.40/update/RTL8125B-5.1.16.tar.gz"
+PKG_URL="https://github.com/awesometic/realtek-r8125-dkms/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
-PKG_LONGDESC="Realtec RTL8125 driver for Linux kernel 5.1.16"
+PKG_LONGDESC="Realtek RTL8125 driver for Linux"
 PKG_IS_KERNEL_PKG="yes"
 
-pre_make_target() {
-  unset LDFLAGS
+make_target() {
+  kernel_make V=1 \
+       -C $(kernel_path) \
+       M="$PKG_BUILD/src"
 }
-
-#make_target() {
-#  kernel_make KDIR=$(kernel_path)
-# cd $PKG_BUILD
-#   bash autorun.sh
-#  make V=1 \
-#       ARCH=$TARGET_KERNEL_ARCH \
-#       KSRC=$(kernel_path) \
-#       CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
-#       CONFIG_POWER_SAVING=n
-#}
 
 makeinstall_target() {
  cd $PKG_BUILD
